e016692c687a3626878595c4cd6239e9f96aca82
Paul Shpilsher <paul.shpilsher@gmail.com>
2024-11-14 15:26:44 +0300

    add thumbnail to initial content

--- core-domain/initial-content/domain.go
+++ core-domain/initial-content/domain.go
@@ -3,7 +3,8 @@ package initialcontent
 import "github.com/gocql/gocql"
 
 type InitialContent struct {
-	ID       gocql.UUID `json:"id"`
-	Kind     string     `json:"kind"`
-	Location string     `json:"location"`
+	ID        gocql.UUID `json:"id"`
+	Kind      string     `json:"kind"`
+	Location  string     `json:"location"`
+	Thumbnail string     `json:"thumbnail"`
 }
